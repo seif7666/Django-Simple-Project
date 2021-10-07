43d0eed455a39d8c3af677c263b121d1d9012568
seif7666 <seifeldin7666@gmail.com>
2021-10-07 15:58:54 +0200

    Project modified to add votes

--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,38 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.http.response import Http404
-from polls.models import Question
-from django.shortcuts import render
+from django.urls import reverse
+from polls.models import Question,Choice
+from django.shortcuts import render,get_object_or_404
 
 
 
 def index(request):
-    questions= Question.getAllQuestions()
-    res=''
-    for i in questions:
-        res += str(i)
-        res +='\n'
-    return HttpResponse(res)
+    questions= Question.objects.all()
+    print(questions[0].id)
+    return HttpResponse(render(request, 'polls/anchor.html' , {'questions':questions}))
 
 def detail(request, question_id):
-    question= Question.getQuestionById(2)
-    question= {'text':question, 'id':1}
-    return HttpResponse(render(request, 'polls/anchor.html', question))
+    question= Question.getQuestionById(question_id)
+    print(question)
+
+    return HttpResponse(render(request, 'polls/index.html', {'question':question}))
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting at question: {question_id}')
+    question= get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/index.html', {
+            'question':question,
+            'error_message' : "You didn't select a choice!"
+        })
+    selected_choice.votes +=1
+    selected_choice.save()
 
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 def results(request, question_id):
-    return HttpResponse(f'Result {question_id}') 
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
 def owner(request):
     return HttpResponse("Hello, world. 819ba59c is the polls index.")   
